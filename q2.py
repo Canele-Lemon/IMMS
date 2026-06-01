445484b79d88affd4f53df1fd23fe35add786d23
Canele-Lemon <gguguggaga@gmail.com>
2026-06-01 17:26:07 +0900

    q2.py 업데이트

--- q2.py
+++ q2.py
@@ -1,77 +1,60 @@
-===== SV API Request Debug =====
-SoundNoisePage.js:3122 model.id: 3295
-SoundNoisePage.js:3123 model.measured_site: TV_RC_RND
-SoundNoisePage.js:3124 rootPath: TV_RC_RND
-SoundNoisePage.js:3121 ===== SV API Request Debug =====
-SoundNoisePage.js:3122 model.id: 3295
-SoundNoisePage.js:3123 model.measured_site: TV_RC_RND
-SoundNoisePage.js:3124 rootPath: TV_RC_RND
-SoundNoisePage.js:3138 SV API response: 
-{root_path: 'TV_RC_RND', no: '3295', sub_path: '', target_test_type: 'SV', files: Array(1)}
-SoundNoisePage.js:3141 files length: 1
-SoundNoisePage.js:3146 firstFile: 
-{filename: '85NU850BENA.AKRFNCX_DV_1_SV_KOREA_00.00.00_2026052…NOTE-NOTE-NOTE-NOTE-NOTE-NOTE_ferdi.almahi_OK.xls', object_name: 'DB2DB/TV_RC_RND/3295/85NU850BENA.AKRFNCX_DV_1_SV_K…NOTE-NOTE-NOTE-NOTE-NOTE-NOTE_ferdi.almahi_OK.xls', number_info: Array(5)}
-SoundNoisePage.js:3147 number_info: 
-(5) [{…}, {…}, {…}, {…}, {…}]
-SoundNoisePage.js:3138 SV API response: 
-{root_path: 'TV_RC_RND', no: '3295', sub_path: '', target_test_type: 'SV', files: Array(1)}
-files
-: 
-[{…}]
-no
-: 
-"3295"
-root_path
-: 
-"TV_RC_RND"
-sub_path
-: 
-""
-target_test_type
-: 
-"SV"
-[[Prototype]]
-: 
-Object
-SoundNoisePage.js:3141 files length: 1
-SoundNoisePage.js:3146 firstFile: 
-{filename: '85NU850BENA.AKRFNCX_DV_1_SV_KOREA_00.00.00_2026052…NOTE-NOTE-NOTE-NOTE-NOTE-NOTE_ferdi.almahi_OK.xls', object_name: 'DB2DB/TV_RC_RND/3295/85NU850BENA.AKRFNCX_DV_1_SV_K…NOTE-NOTE-NOTE-NOTE-NOTE-NOTE_ferdi.almahi_OK.xls', number_info: Array(5)}
-filename
-: 
-"85NU850BENA.AKRFNCX_DV_1_SV_KOREA_00.00.00_20260528-1337_NOTE-NOTE-NOTE-NOTE-NOTE-NOTE_ferdi.almahi_OK.xls"
-number_info
-: 
-(5) [{…}, {…}, {…}, {…}, {…}]
-object_name
-: 
-"DB2DB/TV_RC_RND/3295/85NU850BENA.AKRFNCX_DV_1_SV_KOREA_00.00.00_20260528-1337_NOTE-NOTE-NOTE-NOTE-NOTE-NOTE_ferdi.almahi_OK.xls"
-[[Prototype]]
-: 
-Object
-SoundNoisePage.js:3147 number_info: 
-(5) [{…}, {…}, {…}, {…}, {…}]
-0
-: 
-{number: 1, sample: 3, sheet_name: 'Raw_data (1)', data: {…}}
-1
-: 
-{number: 2, sample: 4, sheet_name: 'Raw_data (2)', data: {…}}
-2
-: 
-{number: 3, sample: 10, sheet_name: 'Raw_data (3)', data: {…}}
-3
-: 
-{number: 4, sample: 11, sheet_name: 'Raw_data (4)', data: {…}}
-4
-: 
-{number: 5, sample: 13, sheet_name: 'Raw_data (5)', data: {…}}
-length
-: 
-5
-[[Prototype]]
-: 
-Array(0)
+function buildSVChartDataFromApi(numberInfoList, selectedSample) {
+  console.log("selectedSample:", selectedSample);
+  console.log(
+    "api sample list:",
+    numberInfoList.map((item) => item.sample)
+  );
 
-이거 데이터가 안뜨는데 왜이렇까요 -> 엑셀에 데이터 정상적인데도 불구하고 No data to display 라고 뜸.
+  const target = numberInfoList.find(
+    (item) => String(item.sample) === String(selectedSample)
+  );
 
-또 가끔 한 pk의 SV/SS버튼 누른 다음에 다른 pk의 SS/SV 버튼을 누르면 샘플 0으로 뜨고 (업데이트x) 다시한번 버튼 눌러야 정상적으로 나오는데 왜이러는 걸까요?
+  console.log("matched target:", target);
+
+  if (!target?.data) return [];
+
+  const preq = target.data.preq ?? [];
+  const spec = target.data.spec ?? [];
+  const rattle = target.data.rattle ?? [];
+
+  console.log("preq length:", preq.length);
+  console.log("spec length:", spec.length);
+  console.log("rattle length:", rattle.length);
+  console.log("first:", preq[0], spec[0], rattle[0]);
+
+  ...
+}
+
+const preq = target.data.preq ?? target.data.freq ?? [];
+
+
+const handleOpenDetail = async (model, testCase) => {
+  setSelectedModel(model);
+  setSelectedTestCase(testCase);
+  setSelectedSample("");
+  setIsDetailOpen(true);
+
+  setSampleInfo([]);
+  setBigData([]);
+  setAttachments([]);
+  setExcelData([]);
+  setSvRawNumberInfo([]);
+  setSvHoverPoint(null);
+
+  await fetchModelData(model);
+};
+
+onClick={(e) => {
+  e.stopPropagation();
+  handleOpenDetail(model, "Sound Vibration");
+}}
+
+onClick={(e) => {
+  e.stopPropagation();
+  handleOpenDetail(model, "Stick-Slip");
+}}
+
+setSelectedModel({
+  ...model,
+  sample_count: sampleCount,
+});
