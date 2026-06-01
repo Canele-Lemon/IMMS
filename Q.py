b3a29d9cddcf00dda0318c436834688e5b0ab823
Canele-Lemon <gguguggaga@gmail.com>
2026-06-01 16:47:18 +0900

    Update Q.py

--- Q.py
+++ Q.py
@@ -1,51 +1,80 @@
-===== SV API Request Debug =====
-SoundNoisePage.js:3169 model.id: 3299
-SoundNoisePage.js:3170 model.measured_site: TV_RC_DQA
-SoundNoisePage.js:3171 rootPath: TV_RC_RND
-SoundNoisePage.js:3185 SV API response: {root_path: 'TV_RC_RND', no: '3299', sub_path: '', target_test_type: 'SV', files: Array(1)}files: [{…}]no: "3299"root_path: "TV_RC_RND"sub_path: ""target_test_type: "SV"[[Prototype]]: Object
-SoundNoisePage.js:3188 files length: 1
-SoundNoisePage.js:3193 firstFile: {filename: '50NU850BPUA.AUSJPCX_PV_1_SV_NA_00.00.00_20260528-1…)-NOTE-(STBY)-NOTE-NOTE-NOTE_guftha.anyoga_NG.xls', object_name: 'DB2DB/TV_RC_RND/3299/50NU850BPUA.AUSJPCX_PV_1_SV_N…)-NOTE-(STBY)-NOTE-NOTE-NOTE_guftha.anyoga_NG.xls', number_info: Array(4)}filename: "50NU850BPUA.AUSJPCX_PV_1_SV_NA_00.00.00_20260528-1909_(STBY)-NOTE-(STBY)-NOTE-NOTE-NOTE_guftha.anyoga_NG.xls"number_info: (4) [{…}, {…}, {…}, {…}]object_name: "DB2DB/TV_RC_RND/3299/50NU850BPUA.AUSJPCX_PV_1_SV_NA_00.00.00_20260528-1909_(STBY)-NOTE-(STBY)-NOTE-NOTE-NOTE_guftha.anyoga_NG.xls"[[Prototype]]: Object
-SoundNoisePage.js:3194 number_info: 
+{
+  "root_path": "TV_RC_RND",
+  "no": "3043",
+  "sub_path": "",
+  "target_test_type": "SV",
+  "files": [
+    {
+      "filename": "50QNED8SBUA.AUSSLCX_DV_1_SV_26Y_NA_20260313-2107_9-25-23-14-12-10_ulya.aqilah_NG.xls",
+      "object_name": "DB2DB/TV_RC_RND/3043/50QNED8SBUA.AUSSLCX_DV_1_SV_26Y_NA_20260313-2107_9-25-23-14-12-10_ulya.aqilah_NG.xls",
+      "number_info": [
+        {
+          "number": 1,
+          "sample": 10,
+          "sheet_name": "Raw_data (1)",
+          "data": {
+            "preq": [
+              61.55,
+              64.69,
 
+curl -X 'GET' \
+  'https://imms-be--8000--minyoung1-park.apps.hedej.lge.com/sound/file/ss/raw-data?root_path=TV_RC_RND&no=3297&target_test_type=SS' \
+  -H 'accept: application/json'
 
-===== SV Chart Build Debug =====
-SoundNoisePage.js:220 selectedSample: 119
-SoundNoisePage.js:221 numberInfo numbers: 
-(4) [1, 2, 3, 4]
-0
-: 
-1
-1
-: 
-2
-2
-: 
-3
-3
-: 
-4
-length
-: 
-4
-[[Prototype]]
-: 
-Array(0)
-SoundNoisePage.js:225 numberInfo sheet names: 
-(4) ['Raw_data (1)', 'Raw_data (2)', 'Raw_data (3)', 'Raw_data (4)']
-0
-: 
-"Raw_data (1)"
-1
-: 
-"Raw_data (2)"
-2
-: 
-"Raw_data (3)"
-3
-: 
-"Raw_data (4)"
-length
-: 
-4
+이렇게 보내면
 
-콘솔에 이렇게 찍히는데 그럼 문제 원인이 실제 sample 명하고 rawdata #1 ... 이 애들이 서로 매핑이 안되어서 그런건가요?
+{
+  "root_path": "TV_RC_RND",
+  "no": "3297",
+  "sub_path": "",
+  "target_test_type": "SS",
+  "files": [
+    {
+      "filename": "50NU850BPUA.AUSJFCX_PV_1_SS_NA_00.00.00_20260528-1728_(STBY)-NOTE-NOTE-NOTE-(STBY)-NOTE_guftha.anyoga_OK.xls",
+      "object_name": "DB2DB/TV_RC_RND/3297/50NU850BPUA.AUSJFCX_PV_1_SS_NA_00.00.00_20260528-1728_(STBY)-NOTE-NOTE-NOTE-(STBY)-NOTE_guftha.anyoga_OK.xls",
+      "summary": {
+        "no1": {
+          "sample": 949,
+          "data": [
+            {
+              "no": 1,
+              "time": 2,
+              "front_of_tv": 34.28,
+              "mirror_effect": 34.58,
+              "max_ch": "Ch 1",
+              "max_spl": 34.28,
+              "limit_level": 25
+            },
+            {
+              "no": 2,
+              "time": 3,
+              "front_of_tv": 26.78,
+              "mirror_effect": 27,
+              "max_ch": "Ch 1",
+              "max_spl": 26.78,
+              "limit_level": 25
+            }
+          ]
+        }
+      },
+      "raw_data": {
+        "no1": {
+          "warm_up": {
+            "time": [
+              1,
+              2,
+              ...
+            ],
+            "front": [
+              -33.55,
+              34.28,
+              ...
+              16.65,
+              16.86
+            ],
+            "rear": [
+              -33.45,
+              35.09, 
+              
+              
+              
