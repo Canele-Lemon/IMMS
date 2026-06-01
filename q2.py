ba8c1ed5d2027f043e206a8cb9452fffb054cd68
Canele-Lemon <gguguggaga@gmail.com>
2026-06-01 17:22:58 +0900

    Update q2.py

--- q2.py
+++ q2.py
@@ -1,93 +1,77 @@
-function calculateSampleCount(model_PK, sample_list) {
-  const summaryRows = sample_list.filter(
-    (s) => s.Model_PK === model_PK && s.Sample_No === "Summary"
-  );
+===== SV API Request Debug =====
+SoundNoisePage.js:3122 model.id: 3295
+SoundNoisePage.js:3123 model.measured_site: TV_RC_RND
+SoundNoisePage.js:3124 rootPath: TV_RC_RND
+SoundNoisePage.js:3121 ===== SV API Request Debug =====
+SoundNoisePage.js:3122 model.id: 3295
+SoundNoisePage.js:3123 model.measured_site: TV_RC_RND
+SoundNoisePage.js:3124 rootPath: TV_RC_RND
+SoundNoisePage.js:3138 SV API response: 
+{root_path: 'TV_RC_RND', no: '3295', sub_path: '', target_test_type: 'SV', files: Array(1)}
+SoundNoisePage.js:3141 files length: 1
+SoundNoisePage.js:3146 firstFile: 
+{filename: '85NU850BENA.AKRFNCX_DV_1_SV_KOREA_00.00.00_2026052…NOTE-NOTE-NOTE-NOTE-NOTE-NOTE_ferdi.almahi_OK.xls', object_name: 'DB2DB/TV_RC_RND/3295/85NU850BENA.AKRFNCX_DV_1_SV_K…NOTE-NOTE-NOTE-NOTE-NOTE-NOTE_ferdi.almahi_OK.xls', number_info: Array(5)}
+SoundNoisePage.js:3147 number_info: 
+(5) [{…}, {…}, {…}, {…}, {…}]
+SoundNoisePage.js:3138 SV API response: 
+{root_path: 'TV_RC_RND', no: '3295', sub_path: '', target_test_type: 'SV', files: Array(1)}
+files
+: 
+[{…}]
+no
+: 
+"3295"
+root_path
+: 
+"TV_RC_RND"
+sub_path
+: 
+""
+target_test_type
+: 
+"SV"
+[[Prototype]]
+: 
+Object
+SoundNoisePage.js:3141 files length: 1
+SoundNoisePage.js:3146 firstFile: 
+{filename: '85NU850BENA.AKRFNCX_DV_1_SV_KOREA_00.00.00_2026052…NOTE-NOTE-NOTE-NOTE-NOTE-NOTE_ferdi.almahi_OK.xls', object_name: 'DB2DB/TV_RC_RND/3295/85NU850BENA.AKRFNCX_DV_1_SV_K…NOTE-NOTE-NOTE-NOTE-NOTE-NOTE_ferdi.almahi_OK.xls', number_info: Array(5)}
+filename
+: 
+"85NU850BENA.AKRFNCX_DV_1_SV_KOREA_00.00.00_20260528-1337_NOTE-NOTE-NOTE-NOTE-NOTE-NOTE_ferdi.almahi_OK.xls"
+number_info
+: 
+(5) [{…}, {…}, {…}, {…}, {…}]
+object_name
+: 
+"DB2DB/TV_RC_RND/3295/85NU850BENA.AKRFNCX_DV_1_SV_KOREA_00.00.00_20260528-1337_NOTE-NOTE-NOTE-NOTE-NOTE-NOTE_ferdi.almahi_OK.xls"
+[[Prototype]]
+: 
+Object
+SoundNoisePage.js:3147 number_info: 
+(5) [{…}, {…}, {…}, {…}, {…}]
+0
+: 
+{number: 1, sample: 3, sheet_name: 'Raw_data (1)', data: {…}}
+1
+: 
+{number: 2, sample: 4, sheet_name: 'Raw_data (2)', data: {…}}
+2
+: 
+{number: 3, sample: 10, sheet_name: 'Raw_data (3)', data: {…}}
+3
+: 
+{number: 4, sample: 11, sheet_name: 'Raw_data (4)', data: {…}}
+4
+: 
+{number: 5, sample: 13, sheet_name: 'Raw_data (5)', data: {…}}
+length
+: 
+5
+[[Prototype]]
+: 
+Array(0)
 
-  const hasStickSlipSummary = summaryRows.some(
-    (s) => normalizeItem(s.Item) === "Stick-Slip"
-  );
+이거 데이터가 안뜨는데 왜이렇까요 -> 엑셀에 데이터 정상적인데도 불구하고 No data to display 라고 뜸.
 
-  const hasSoundVibrationSummary = summaryRows.some(
-    (s) => normalizeItem(s.Item) === "Sound Vibration"
-  );
-
-  const sampleRows = sample_list.filter(
-    (s) => s.Model_PK === model_PK && s.Sample_No !== "Summary"
-  );
-
-  const stickSlipSamples = hasStickSlipSummary
-    ? new Set(
-        sampleRows
-          .filter((s) => normalizeItem(s.Item) === "Stick-Slip")
-          .map((s) => s.Sample_No)
-      ).size
-    : 0;
-
-  const soundVibrationSamples = hasSoundVibrationSummary
-    ? new Set(
-        sampleRows
-          .filter((s) => normalizeItem(s.Item) === "Sound Vibration")
-          .map((s) => s.Sample_No)
-      ).size
-    : 0;
-
-  return {
-    total: stickSlipSamples + soundVibrationSamples,
-    stick_slip: stickSlipSamples,
-    sound_vibration: soundVibrationSamples,
-    has_stick_slip_summary: hasStickSlipSummary,
-    has_sound_vibration_summary: hasSoundVibrationSummary,
-  };
-}
-
-
-const fetchValidModelPKs = async () => {
-  const res = await fetch(
-    `${BASE_URL()}/sound/db/vibration-sample-info?limit=5000`
-  );
-  const data = await res.json();
-
-  const validRows = data.filter(
-    (s) =>
-      normalizeItem(s.Item) === "Stick-Slip" ||
-      normalizeItem(s.Item) === "Sound Vibration"
-  );
-
-  const modelMap = new Map();
-
-  validRows.forEach((s) => {
-    if (!modelMap.has(s.Model_PK)) {
-      modelMap.set(s.Model_PK, []);
-    }
-    modelMap.get(s.Model_PK).push(s);
-  });
-
-  return modelMap;
-};
-
-
-
-function hasStickSlip(model) {
-  return (
-    model.sample_count.has_stick_slip_summary === true &&
-    model.sample_count.stick_slip > 0
-  );
-}
-
-function hasSoundVibration(model) {
-  return (
-    model.sample_count.has_sound_vibration_summary === true &&
-    model.sample_count.sound_vibration > 0
-  );
-}
-
-
-
-
-Sample_No === "Summary" && Item === "Sound Vibration"
-→ SV 측정 모델로 인정
-→ 그다음 Sample_No !== "Summary"인 SV sample 개수 count
-
-Sample_No === "Summary" && Item === "Stick-Slip" 또는 "Stic-Slip"
-→ SS 측정 모델로 인정
-→ 그다음 Sample_No !== "Summary"인 SS sample 개수 count
+또 가끔 한 pk의 SV/SS버튼 누른 다음에 다른 pk의 SS/SV 버튼을 누르면 샘플 0으로 뜨고 (업데이트x) 다시한번 버튼 눌러야 정상적으로 나오는데 왜이러는 걸까요?
